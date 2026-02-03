ed18fe53250ebd7cc66f45b763c1cf6914a72e79
breno manoel <breno24.caixeta@gmail.com>
2026-02-03 13:11:59 -0300

    feat: add request and response DTOs for User

--- src/main/java/com/nexus/game_trade/dto/DtoUser.java
+++ src/main/java/com/nexus/game_trade/dto/DtoUser.java
@@ -1,4 +1,24 @@
 package com.nexus.game_trade.dto;
 
+import com.nexus.game_trade.model.entities.EnumUsers;
+
+import java.util.UUID;
+
 public class DtoUser {
+
+    public record UserRequestDTO(
+            String nickname,
+            String email,
+            String password,
+            EnumUsers level
+    ) {}
+
+    public record UserResponseDTO(
+            UUID id,
+            String nickname,
+            String email,
+            Double avgReputation,
+            Integer totalExchanges,
+            EnumUsers level
+    ) {}
 }
