82e785d18fd58412ff0fe359bb65fe7a993a8f92
breno manoel <breno24.caixeta@gmail.com>
2026-02-03 13:52:04 -0300

    feat: add bean validation to UserDTO

--- src/main/java/com/nexus/game_trade/dto/DtoUser.java
+++ src/main/java/com/nexus/game_trade/dto/DtoUser.java
@@ -1,15 +1,27 @@
 package com.nexus.game_trade.dto;
 
 import com.nexus.game_trade.model.entities.EnumUsers;
+import jakarta.validation.constraints.*;
+import lombok.Value;
 
 import java.util.UUID;
 
 public class DtoUser {
 
     public record UserRequestDTO(
+            @NotBlank(message = "Name is required")
+            @Size(min = 3, max = 100, message = "The name must be between 3 and 100 characters long.")
             String nickname,
+
+            @Email(message = "Email invalid")
+            @NotBlank(message = "Email is required")
             String email,
+
+            @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$",
+                    message = "The password must be at least 8 characters long, with 1 letter and 1 number.")
             String password,
+
+            @NotBlank(message = "level is required")
             EnumUsers level
     ) {}
 
---
+++ src/test/java/com/nexus/game_trade/GameTradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexus.game_trade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameTradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
