daa2931625b1c81620bf3688124a5241e028d250
breno manoel <breno24.caixeta@gmail.com>
2026-02-03 13:13:04 -0300

    feat: add UserRepository with JPA methods

--- src/main/java/com/nexus/game_trade/repository/RepositoryUser.java
+++ src/main/java/com/nexus/game_trade/repository/RepositoryUser.java
@@ -1,12 +1,16 @@
 package com.nexus.game_trade.repository;
 
-import com.nexus.game_trade.model.entities.Users;
+import com.nexus.game_trade.model.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface RepositoryUsers extends JpaRepository<Users, UUID> {
+@Repository
+public interface RepositoryUser extends JpaRepository<User, UUID> {
 
-    Users createUser();
-    
+    Optional<User> findByNickname(String nickname);
+
 }
